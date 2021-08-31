bb4830e0f8c5268d2a7da6d824575c7c7b52ba09
abdurztc <abdurztc@gmail.com>
2021-09-01 02:08:27 +0700

    Setup Sign In Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,7 +85,7 @@ export default function Home() {
                 <li className="nav-item my-auto">
                   <a
                     className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-                    href="./src/sign-in.html"
+                    href="/sign-in"
                     role="button"
                   >
                     Sign In
