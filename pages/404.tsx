791c204054693fab923bd5574e1e7bd8e7ca9be5
abdurztc <abdurztc@gmail.com>
2021-09-01 02:29:33 +0700

    Custom 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -189,7 +189,7 @@ export default function Custom404() {
                     tidak tersedia pada sistem kami dan menghubungi</p>
             </div>
             <div className="button-group d-flex flex-column mx-auto">
-                <a className="btn btn-homepage fw-medium text-lg text-white rounded-pill" href="#"
+                <a className="btn btn-homepage fw-medium text-lg text-white rounded-pill" href="/#"
                     role="button">Homepage</a>
             </div>
         </div>
