e78209508f4ac33a666d8c4c3102633bee58d1e9
abdurztc <abdurztc@gmail.com>
2021-09-19 05:09:44 +0700

    Member Transactions Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import '../styles/sign-up.css';
 import '../styles/sign-up-photo.css';
 import '../styles/sidebar.css';
 import '../styles/overview.css';
+import '../styles/transactions.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
