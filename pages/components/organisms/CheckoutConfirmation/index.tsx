aaaeac6c79012cacdc0cfa86aa78cbbc25e8cece
abdurztc <abdurztc@gmail.com>
2022-03-27 20:47:16 +0700

    Refactoring Checkout Page

---
+++ pages/components/organisms/CheckoutConfirmation/index.tsx
@@ -0,0 +1,21 @@
+export default function CheckoutConfirmation() {
+  return (
+      <>
+      <label className="checkbox-label text-lg color-palette-1">
+          I have transferred the money
+          <input type="checkbox" />
+          <span className="checkmark" />
+        </label>
+        <div className="d-md-block d-flex flex-column w-100 pt-50">
+          <a
+            className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
+            href="/complete-checkout"
+            role="button"
+          >
+            Confirm Payment
+          </a>
+        </div>
+      </>
+    
+  )
+}
--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -1,9 +1,12 @@
+import Footer from "../components/organisms/Footer";
+import Navbar from "../components/organisms/Navbar";
 import TopUpForm from "../components/organisms/TopUpForm";
 import TopUpItem from "../components/organisms/TopUpItem";
 
 export default function Detail() {
   return (
-    // <!-- Detail Content -->
+    <>
+    <Navbar />
     <section className="detail pt-lg-60 pb-50">
       <div className="container-xxl container-fluid">
         <div className="detail-header pb-50">
@@ -23,5 +26,7 @@ export default function Detail() {
         </div>
       </div>
     </section>
+    <Footer />
+    </>
   );
 }
